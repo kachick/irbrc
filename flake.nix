36ba1719b06ae0e0b82d5cead17aaf471c699841
Kenichi Kamiya <kachick1@gmail.com>
2026-01-14 20:15:17 +0900

    Update flake channel to 25.11

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs =
@@ -13,7 +13,7 @@
       forAllSystems = lib.genAttrs lib.systems.flakeExposed;
     in
     {
-      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
       devShells = forAllSystems (
         system:
         let
@@ -27,7 +27,8 @@
               [
                 bashInteractive
                 findutils # xargs
-                nixfmt-rfc-style
+                nixfmt
+                nixfmt-tree
                 nil
 
                 ruby_3_4
